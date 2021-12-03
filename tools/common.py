e282b5094d9621ffc95e63f097468e9d722438e2
Seb Seager <seb.seager@gmail.com>
2021-12-03 16:38:50 -0500

    Added plot styling func

--- tools/common.py
+++ tools/common.py
@@ -22,3 +22,55 @@ def log(msg, lvl=0, quiet=False):
     print(f"{prefix}{msg}")
     if lvl == 2:
         sys.exit(1)
+
+
+def style_ax(ax, xlab="", ylab="", aspect="auto", xlim=None, ylim=None):
+    for axis in ("top", "bottom", "left", "right"):
+        # hide axes
+        if axis in ("top", "right"):
+            ax.spines["top"].set_visible(False)
+            ax.spines["right"].set_visible(False)
+        else:
+            ax.spines["left"].set_visible(True)
+            ax.spines["bottom"].set_visible(True)
+
+        # general formatting
+        ax.spines[axis].set_linewidth(1.0)
+        ax.set_xlabel(xlab, fontsize=14)
+        ax.set_ylabel(ylab, fontsize=14)
+        ax.tick_params(
+            axis="x",
+            which="both",
+            bottom=True,
+            top=False,
+            direction="out",
+            length=4.0,
+            width=1.0,
+            color="k",
+            labelsize=12,
+        )
+        ax.tick_params(
+            axis="y",
+            which="both",
+            left=False,
+            right=False,
+            direction="out",
+            length=4.0,
+            width=1.0,
+            color="k",
+            labelsize=12,
+        )
+        ax.grid(color="gray", ls=":", lw=0.5)
+
+        # set aspect ratio
+        # values can be "auto", "equal" (same as 1), or a float
+        ax.set_aspect(aspect)
+
+        # set x and y limits
+        if xlim is not None:
+            ax.set_xlim(xlim)
+        if ylim is not None:
+            ax.set_ylim(ylim)
+
+        # useful for chaining (is ax modified in place?)
+        return ax
