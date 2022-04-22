19259ad8fe44c4a20c10007158c4489f6fdde0dc
Seb Seager <seb.seager@gmail.com>
2022-04-22 12:09:10 -0400

    Allow file skipping

--- scripts/mrc_to_img.py
+++ scripts/mrc_to_img.py
@@ -7,47 +7,7 @@ from PIL import Image
 from pathlib import Path
 import mrcfile
 import warnings
-
-
-# utils
-
-
-def _log(msg, lvl=0, quiet=False):
-    """Format and print message to console with one of the following logging levels:
-    0: info (print and continue execution; ignore if quiet=True)
-    1: warning (print and continue execution)
-    2: error (print and exit with code 1)
-    """
-
-    if lvl == 0 and quiet:
-        return
-
-    prefix = ""
-    if lvl == 0:
-        prefix = "INFO: "
-    elif lvl == 1:
-        prefix = "WARN: "
-    elif lvl == 2:
-        prefix = "CRITICAL: "
-
-    print(f"{prefix}{msg}")
-    if lvl == 2:
-        sys.exit(1)
-
-
-def _read_mrc(path, mmap=False):
-    """Use the mrcfile module to read data from micrograph at given path."""
-
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        if mmap:
-            with mrcfile.mmap(path, mode="r", permissive=True) as f:
-                mrc = f.data
-        else:
-            with mrcfile.open(path, mode="r", permissive=True) as f:
-                mrc = f.data
-
-    return mrc
+from common import *
 
 
 def _save_to_img(arr, save_path=None, force=False):
@@ -55,7 +15,7 @@ def _save_to_img(arr, save_path=None, force=False):
     png = Image.fromarray(scaled)
     if save_path:
         if Path(save_path).is_file() and not force:
-            _log("re-run with the force flag to allow overwriting", 2)
+            log("re-run with the force flag to allow overwriting", 2)
         png.save(save_path)
     return png
 
@@ -67,7 +27,7 @@ if __name__ == "__main__":
     parser.add_argument("mrc", help="Micrograph file(s) to convert to PNG", nargs="+")
     parser.add_argument(
         "-o",
-        help="Output directory for new PNG files",
+        help="Output directory for new PNG files (will be created if it does not exist)",
         required=True,
     )
     parser.add_argument(
@@ -79,20 +39,20 @@ if __name__ == "__main__":
     parser.add_argument(
         "--force",
         action="store_true",
-        help="Allow files in output directory to be overwritten and make output "
-        "directory if it does not exist",
+        help="Allow files in output directory to be overwritten",
     )
 
     a = parser.parse_args()
-    a.mrc = np.atleast_1d(a.mrc).tolist()
-    a.o = Path(a.o).resolve()
-
-    if a.force:
-        a.o.mkdir(parents=True, exist_ok=True)
+    a.mrc = [norm_path(p) for p in np.atleast_1d(a.mrc).tolist()]
+    a.o = norm_path(a.o)
+    a.o.mkdir(parents=True, exist_ok=True)
 
     for mrc_path in tqdm(a.mrc):
-        mrc = _read_mrc(Path(mrc_path).resolve())
-        out_path = os.path.join(a.o, Path(mrc_path).stem + f".{a.f}")
+        out_path = a.o / mrc_path.stem + "." + a.f
+        if out_path.is_file() and not a.force:
+            log(f"skipping {mrc_path} (re-run with --force to allow overwriting)")
+            continue
+        mrc = read_mrc(Path(mrc_path).resolve())
         _save_to_img(mrc, save_path=out_path, force=a.force)
 
-    _log(f"Done. Output is in {a.o}")
+    log(f"Done. Output is in {a.o}")
